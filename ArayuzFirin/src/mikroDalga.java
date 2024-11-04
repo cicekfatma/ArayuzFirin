d48cd6f5998e0bdf4b177ca07e3be4a70a082df3
Fatma Çiçek <153107444+cicekfatma@users.noreply.github.com>
2024-11-04 17:53:29 +0300

    Add files via upload

---
+++ ArayuzFirin/src/mikroDalga.java
@@ -0,0 +1,10 @@
+public class mikroDalga implements Firin{
+    @Override
+    public void rafSayisi(){
+        System.out.println("4 raf bulunuyor.");
+    }
+    @Override
+    public boolean TurboVarmi(){
+        return true;
+    }
+}
